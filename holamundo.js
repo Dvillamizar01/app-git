fd20190389643d1eb3b979467f4c6be01413060f
victhormartinez <Martinez.ing21@gmail.com>
2023-06-21 21:45:55 -0500

    Update holamundo.js

--- holamundo.js
+++ holamundo.js
@@ -2,4 +2,11 @@ const express = require('express');
 const holamundo = express();
 holamundo.get('/', (req, res) =>
 res.send('Hola Mundo!'));
+
+holamundo.get('/sumar/:num1/:num2', (req, res) => {
+  const num1 = parseInt(req.params.num1);
+  const num2 = parseInt(req.params.num2);
+  const resultado = num1 + num2;
+  res.send(`El resultado de la suma es: ${resultado}`);
+})
 module.exports = holamundo;
